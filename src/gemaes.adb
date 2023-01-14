e23a4b64f5c2dac84ed39a38d879c33d4cf0bf18
Markus Fröschle <mfro@mubf.de>
2023-01-14 08:12:38 +0100

    more binding

--- src/gemaes.adb
+++ src/gemaes.adb
@@ -7,10 +7,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 package body GemAes is
    type uint16_array_type is array(integer range <>) of uint16;
-   type uint16_array_access is access all uint16_array_type;
-
    type address_array_type is array(integer range <>) of System.Address;
-   type address_array_access is access all address_array_type;
 
    type aespb_type is record
       pcontrol,
@@ -21,13 +18,13 @@ package body GemAes is
       padrout      : System.Address;
    end record;
 
-   control,
-   global,
-   intin,
-   intout         : aliased uint16_array_type(0 .. 127) := (others => 0);
+   control        : uint16_array_type(0 .. 4);
+   global         : uint16_array_type(0 .. 14);
+   intin          : uint16_array_type(0.. 15);
+   intout         : uint16_array_type(0 .. 9) := (others => 0);
 
-   addrin,
-   addrout         : aliased address_array_type(0 .. 4);
+   addrin         : address_array_type(0 .. 7);
+   addrout        : address_array_type(0 .. 1);
 
    aespb : aespb_type := aespb_type'(control'Address, global'Address,
                                      intin'Address, intout'Address,
@@ -38,9 +35,7 @@ package body GemAes is
       pragma inline(Application_Init);
       function to_address is new Ada.Unchecked_Conversion(System.Address, Unsigned_32);
    begin
-      control := (others => 0);
-      control(0) := 10;     -- appl_init()
-      control(2) := 1;      -- elements in int_out
+      control := (0 => 10, 2 => 1, others => 0);
       Asm("move.l       %0,d1"          & LF & HT &
           "move.w       #200,d0"        & LF & HT &
           "trap         #2"             & LF & HT,
@@ -54,9 +49,7 @@ package body GemAes is
       pragma inline(Application_Exit);
       function to_address is new Ada.Unchecked_Conversion(System.Address, Unsigned_32);
    begin
-      control := (others => 0);
-      control(0) := 19;    -- appl_exit();
-      control(2) := 1;
+      control := (0 => 19, 2 => 1, others => 0);
       Asm("move.l    %0,d1"            & LF & HT &
           "move.w    #200,d0"          & LF & HT &
           "trap      #2"               & LF & HT,
@@ -68,18 +61,13 @@ package body GemAes is
 
    function Form_Alert(default_button : int16; alert_string : string) return int16 is
       use ASCII;
-      -- pragma inline(Form_Alert);
+      pragma inline(Form_Alert);
          
       s : string := alert_string & NUL;
       function to_address is new Ada.Unchecked_Conversion(System.Address, Unsigned_32);
    
    begin
-      control(0) := 52;         -- form_alert()
-      control(1) := 1;          -- elements in int_in
-      control(2) := 1;          -- elements in int_out
-      control(3) := 1;          -- elements in addr_in
-      control(4) := 0;          -- elements in addr_out
-
+      control := (0 => 52, 1 .. 3 => 1, 4 => 0);
       intin(0) := uint16(default_button);
       addrin(0) := s(1)'Address;
 
@@ -92,4 +80,40 @@ package body GemAes is
 
       return int16(intout(0));
    end Form_Alert;
+
+   procedure Application_Bitvector_Set(Floppy_Disk_Vector : uint16; Hard_Disk_Vector : uint16) is
+      use ASCII;
+      pragma inline(Application_Bitvector_Set);
+      function to_address is new Ada.Unchecked_Conversion(System.Address, Unsigned_32);
+   begin
+      control := (0 => 16, 1 => 2, 2 => 1, others => 0);
+      Asm("move.l       %0,d1"         & LF & HT &
+          "move.w       #200,d0"       & LF & HT &
+          "trap         #2"            & LF & HT,
+          Volatile => True, 
+          Inputs => Interfaces.Unsigned_32'Asm_Input("g", to_address(aespb'Address))
+      );
+   end Application_Bitvector_Set;
+
+   function Application_Control(Application_ID : in int16; Cntrl : in Control_Type; ControlOut : out int16) return int16 is
+      use ASCII;
+      pragma inline(Application_Control);
+      function to_address is new Ada.Unchecked_Conversion(System.Address, Unsigned_32);
+   begin
+      control := (0 => 129, 1 => Control_Type'Enum_Rep(Cntrl), 3 => 1, others => 0);
+      Asm("move.l       %0,d1"         & LF & HT &
+          "move.w       #200,d0"       & LF & HT &
+          "trap         #2"            & LF & HT,
+          Volatile => True, 
+          Inputs => Interfaces.Unsigned_32'Asm_Input("g", to_address(aespb'Address))
+      );
+      ControlOut := int16(intout(1));
+      return int16(intout(0));
+   end Application_Control;
+
+   function Application_Find(File_Name : String) return int16 is
+   begin
+      null;
+      return 0;
+   end Application_Find;
 end GemAes;
--- src/gemaes.ads
+++ src/gemaes.ads
@@ -8,4 +8,16 @@ package GemAes is
    function Application_Init return int16;
    function Application_Exit return int16;
    function Form_Alert(default_button : in int16; alert_string : in String) return int16;
+   procedure Application_Bitvector_Set(Floppy_Disk_Vector : uint16; Hard_Disk_Vector : uint16);
+
+   -- constants for Application_Control
+   type Control_Type is (APC_TOPNEXT, APC_KILL, APC_SYSTEM, APC_HIDE, APC_SHOW, APC_TOP,
+                         APC_HIDENOT, APC_INFO, APC_MENU, APC_WIDGETS, APC_APP_CONFIG,
+                         APC_INFORM_MESAG);
+   for Control_Type use (APC_TOPNEXT => 0, APC_KILL => 1, APC_SYSTEM => 2, APC_HIDE => 10,
+                         APC_SHOW => 11, APC_TOP => 12, APC_HIDENOT => 13, APC_INFO => 14,
+                         APC_MENU => 15, APC_WIDGETS => 16, APC_APP_CONFIG => 17,
+                         APC_INFORM_MESAG => 18);
+   function Application_Control(Application_ID : in int16; Cntrl : in Control_Type; ControlOut : out int16) return int16;
+
 end GemAes;
