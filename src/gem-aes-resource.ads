be23838c3c4eee32b0198298a3528b6a638a1316
Markus Fröschle <mfro@mubf.de>
2023-01-22 22:18:31 +0100

    fix indent

--- src/gem-aes-resource.ads
+++ src/gem-aes-resource.ads
@@ -1,43 +1,43 @@
 with GEM.AES; use GEM.AES;
 
 package GEM.AES.Resource is
-    type Resource_Type is (Tree,                -- R_TREE
-                           Object,              -- R_OBJECT
-                           Text_Ed_Info,        -- R_TEDINFO
-                           Icon_Block,          -- R_ICONBLK
-                           Bit_Block,           -- R_BITBLK
-                           R_String,            -- R_STRING
-                           Image_Data,          -- R_IMAGEDATA
-                           Object_Spec,         -- R_OBSPEC
-                           Text_Pointer,        -- R_TEPTEXT
-                           Template_Pointer,    -- R_TEPTMPLT
-                           Valid_Pointer,       -- R_TEPVALID
-                           Icon_Mask_Pointer,   -- R_IBPMASK
-                           Icon_Data_Pointer,   -- R_IBPDATA
-                           Icon_Text_Pointer,   -- R_IBPTEXT
-                           Bitmap_Data_Pointer, -- R_BIPDATA
-                           Free_String,         -- R_FRSTR
-                           Free_Image           -- R_FRIMG
-                          );
-    for Resource_Type use (Tree => 0,
-                           Object => 1,
-                           Text_Ed_Info => 2,
-                           Icon_Block => 3,
-                           Bit_Block => 4,
-                           R_String => 5,
-                           Image_Data => 6,
-                           Object_Spec => 7,
-                           Text_Pointer => 8,
-                           Template_Pointer => 9,
-                           Valid_Pointer => 10,
-                           Icon_Mask_Pointer => 11,
-                           Icon_Data_Pointer => 12,
-                           Icon_Text_Pointer => 13,
-                           Bitmap_Data_Pointer => 14,
-                           Free_String => 15,
-                           Free_Image => 16
-                          );
-    function Get_Address(Typ : Resource_Type; Index : Int16; Addr : out System.Address) return Int16;
-    function Free return Int16;
-    function Load(Resource_Name : String) return Int16;
+   type Resource_Type is (Tree,                -- R_TREE
+                          Object,              -- R_OBJECT
+                          Text_Ed_Info,        -- R_TEDINFO
+                          Icon_Block,          -- R_ICONBLK
+                          Bit_Block,           -- R_BITBLK
+                          R_String,            -- R_STRING
+                          Image_Data,          -- R_IMAGEDATA
+                          Object_Spec,         -- R_OBSPEC
+                          Text_Pointer,        -- R_TEPTEXT
+                          Template_Pointer,    -- R_TEPTMPLT
+                          Valid_Pointer,       -- R_TEPVALID
+                          Icon_Mask_Pointer,   -- R_IBPMASK
+                          Icon_Data_Pointer,   -- R_IBPDATA
+                          Icon_Text_Pointer,   -- R_IBPTEXT
+                          Bitmap_Data_Pointer, -- R_BIPDATA
+                          Free_String,         -- R_FRSTR
+                          Free_Image           -- R_FRIMG
+                         );
+   for Resource_Type use (Tree => 0,
+                          Object => 1,
+                          Text_Ed_Info => 2,
+                          Icon_Block => 3,
+                          Bit_Block => 4,
+                          R_String => 5,
+                          Image_Data => 6,
+                          Object_Spec => 7,
+                          Text_Pointer => 8,
+                          Template_Pointer => 9,
+                          Valid_Pointer => 10,
+                          Icon_Mask_Pointer => 11,
+                          Icon_Data_Pointer => 12,
+                          Icon_Text_Pointer => 13,
+                          Bitmap_Data_Pointer => 14,
+                          Free_String => 15,
+                          Free_Image => 16
+                         );
+   function Get_Address(Typ : Resource_Type; Index : Int16; Addr : out System.Address) return Int16;
+   function Free return Int16;
+   function Load(Resource_Name : String) return Int16;
 end GEM.AES.Resource;
--- src/gemaestest.adb
+++ src/gemaestest.adb
@@ -10,28 +10,26 @@ with Ada.Text_IO;
 with OGEM; use OGEM;
 
 procedure GemAESTest is
-    Application_Id      : Int16;
-    Button              : Int16;
-    Physical_Handle     : Int16;
+   Application_Id      : Int16;
+   Button              : Int16;
+   Physical_Handle     : Int16;
 
-    Char_Width,
-    Char_Height,
-    Char_Box_Width,
-    Char_Box_Height     : Int16;
+   Char_Width,
+   Char_Height,
+   Char_Box_Width,
+   Char_Box_Height     : Int16;
 
-    Desk_Rectangle      : GEM.AES.Rectangle;
-
-    Resource_File_Name : constant String := "aestest.rsc";
-
+   Desk_Rectangle      : GEM.AES.Rectangle;
+   Resource_File_Name : constant String := "aestest.rsc";
 begin
-    Application_Id := Application.Init;
-    Physical_Handle := Graf.Handle(Char_Width, Char_Height, Char_Box_Width, Char_Box_Height);
-    Desk_Rectangle := GEM.AES.Window.Get(0, Window.Work_XYWH);
+   Application_Id := Application.Init;
+   Physical_Handle := Graf.Handle(Char_Width, Char_Height, Char_Box_Width, Char_Box_Height);
+   Desk_Rectangle := GEM.AES.Window.Get(0, Window.Work_XYWH);
 
-    if Resource.Load(Resource_File_Name) /= 0 then
-        Button := Form.Alert(3, "[3][Hello from Ada][ OK ]");
-    else
-        Button := Form.Alert(1, "[1][Resource file|" & Resource_File_Name & "|Not found][EXIT]");
-    end if;
-    Application_Id := Application.AExit;
+   if Resource.Load(Resource_File_Name) /= 0 then
+      Button := Form.Alert(3, "[3][Hello from Ada][ OK ]");
+   else
+      Button := Form.Alert(1, "[1][Resource file|" & Resource_File_Name & "|Not found][EXIT]");
+   end if;
+   Application_Id := Application.AExit;
 end GemAESTest;
